bb1d1fbb99269bb6d083d6c14a464dd2e3bd02d9
Zachary Medellin <ZacharyMedellin44@gmail.com>
2021-04-03 00:41:57 -0700

    app page director set up

--- client/src/component/App.jsx
+++ client/src/component/App.jsx
@@ -1,33 +1,65 @@
-import React from 'react'
+import React from 'react';
+import HomePage from './homepage/HomePage.jsx';
+import Login from './login/Login.jsx';
+import FarmersMarket from './marketplace/FarmersMarket.jsx';
+import UserProfile from './userinfo/UserProfile.jsx';
 
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      count: 0
+      page: '',
     }
+    this.changePage = this.changePage.bind(this);
+  }
 
+
+  changePage(e) {
+    console.log(e.target.name)
+    this.setState({
+      page: e.target.name
+    })
   }
 
   render() {
-    if(this.state.count === 0){
     return (
       <div>
-      <h1>TEST1</h1>
-      <button>previous</button>
-        <button>home</button>
-        <button>next</button>
-        </div>
-    )}else if(this.state.count === 1){
-      <div>
-      <h1>TEST2</h1>
-      <button onClick={this.next}>previous</button>
-        <button>home</button>
-        <button>next</button>
-        </div>
-    }
 
+      {this.state.page.length === 0 &&
+        <div>
+        <button name="home" onClick={this.changePage}>HomePage</button>
+        <button name="login" onClick={this.changePage}>Login/Signin</button>
+        <button name="market" onClick={this.changePage}>MarketPlace</button>
+        <button name="profile" onClick={this.changePage}>UserProfile</button>
+        </div>
+        }
+        {this.state.page === "home" && (
+          <HomePage />
+        )}
+
+        {this.state.page === "login" && (
+          <Login />
+        )}
+
+
+        {this.state.page === "market" && (
+          <FarmersMarket />
+        )}
+
+
+        {this.state.page === "profile" && (
+          <UserProfile />
+        )}
+
+
+
+
+
+
+
+      </div>
+    )
   }
 }
 export default App

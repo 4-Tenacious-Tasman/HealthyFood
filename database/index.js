f43e0b1ea87eb0e6c19322257911dbca5157e7e0
Anna Zhu <zeehaychoo@gmail.com>
2021-04-05 11:12:29 -0700

    Privatizing db info

--- database/index.js
+++ database/index.js
@@ -1,18 +1,13 @@
-// const { Pool } = require('pg');
+const { Pool } = require('pg');
 
-// const pool = new Pool({
-//   user: 'post',
-//   host: 'localhost',
-//   database: 'test',
-//   password: 'password',
-//   port: 5432,
-// });
-// pool.connect((err, done) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   console.log('postgresSQL is connected');
-// });
+const pool = new Pool();
 
-// module.exports = pool;
+pool.connect((err, done) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('postgresSQL is connected');
+});
+
+module.exports = pool;

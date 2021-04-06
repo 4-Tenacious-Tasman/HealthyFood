6fbdb367baa282d8835f964dc86a7a21235073fa
Marco Garcia <marcogarcia831@gmail.com>
2021-04-05 22:37:58 -0700

    add userpreferences modal and prepared react to handle api request

--- client/src/component/userinfo/UserProfile.jsx
+++ client/src/component/userinfo/UserProfile.jsx
@@ -1,24 +1,72 @@
 import React from 'react';
-import styles from '/home/marco_ga/HealthyFood/client/src/component/userinfo/Userinfo.module.css';
+import axios from 'axios';
+import Preferances from './UserPreferences.jsx'
+import styles from './Userinfo.module.css';
 
 class UserProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      user: {
+        id: 3,
+        email: 'hahaha@gmail.com',
+        first_name: '',
+        last_name: '',
+        age: null,
+        preferences: {
+          target_calories: null,
+          diet: null,
+          exclude: null
+        }
+      },
+      PreferancesBool: false
 
     }
+    this.updatePreferences = this.updatePreferences.bind(this);
+    this.submitPreferences = this.submitPreferences.bind(this);
+  }
+  updatePreferences() {
+    let modal = !this.state.PreferancesBool;
+    this.setState({
+      PreferancesBool: modal
+    })
+  }
+
+  submitPreferences(usr) {
+    const { first_name, last_name, age, target_calories, diet, exclude } = usr;
+    const excludeString = exclude.join();
+    const email = this.state.user.email;
+    axios.post('/signup', { first_name, last_name, email, age, target_calories, diet, exclude: excludeString })
+      .then(response => {
+        const { data } = response;
+        this.setState({
+          [this.state.user.first_name]: data.first_name,
+          [this.state.user.last_name]: data.last_name,
+          [this.state.user.age]: data.age,
+          [this.state.user.preferences]: {
+            target_calories: data.target_calories,
+            diet: data.diet,
+            exclude: data.exclude
+          }
+        });
+      })
+      .catch(err => {
+        throw err;
+      })
   }
 
   render() {
     return (
       <div >
+        {this.state.PreferancesBool ? <Preferances close={this.updatePreferences} submitPreferences={this.submitPreferences} /> : null}
         <div className={`${styles.photoContainer} container mt-5 d-flex`} >
           <div className={`${styles.userinfo} card p-3`}>
             <img src='https://myspace.com/common/images/user.png' className={`${styles.UserPhoto} rounded`} width="30%" />
             <br></br>
             <p className={styles.greeting}>Hello User</p>
             <br></br>
-            <button className={styles.Preferances} >Edit Preferances</button>
+            <button className={styles.Preferances} onClick={(event) => { event.preventDefault(); this.updatePreferances() }} >Edit Profile</button>
+
           </div>
 
         </div>
@@ -40,33 +88,33 @@ class UserProfile extends React.Component {
               </tr>
             </thead>
             <tbody>
-            <tr className={styles.weeklyMeals}>
-      <td>Meal Mon breakfast</td>
-      <td>Meal Tus breakfast </td>
-      <td>Meal Wed breakfast</td>
-      <td>Meal thur breakfast</td>
-      <td>Meal fri breakfast</td>
-      <td>Meal sat breakfast</td>
-      <td>Meal sun breakfast</td>
-    </tr>
-    <tr>
-    <td>Meal Mon lunch</td>
-      <td>Meal Tus lunch </td>
-      <td>Meal Wed lunch</td>
-      <td>Meal thur lunch</td>
-      <td>Meal fri lunch</td>
-      <td>Meal sat lunch</td>
-      <td>Meal sun lunch</td>
-      </tr>
-      <tr>
-    <td>Meal Mon dinner</td>
-      <td>Meal Tus dinner </td>
-      <td>Meal Wed dinner</td>
-      <td>Meal thur dinner</td>
-      <td>Meal fri dinner</td>
-      <td>Meal sat dinner</td>
-      <td>Meal sun dinner</td>
-      </tr>
+              <tr className={styles.weeklyMeals}>
+                <td>Meal Mon breakfast</td>
+                <td>Meal Tus breakfast </td>
+                <td>Meal Wed breakfast</td>
+                <td>Meal thur breakfast</td>
+                <td>Meal fri breakfast</td>
+                <td>Meal sat breakfast</td>
+                <td>Meal sun breakfast</td>
+              </tr>
+              <tr>
+                <td>Meal Mon lunch</td>
+                <td>Meal Tus lunch </td>
+                <td>Meal Wed lunch</td>
+                <td>Meal thur lunch</td>
+                <td>Meal fri lunch</td>
+                <td>Meal sat lunch</td>
+                <td>Meal sun lunch</td>
+              </tr>
+              <tr>
+                <td>Meal Mon dinner</td>
+                <td>Meal Tus dinner </td>
+                <td>Meal Wed dinner</td>
+                <td>Meal thur dinner</td>
+                <td>Meal fri dinner</td>
+                <td>Meal sat dinner</td>
+                <td>Meal sun dinner</td>
+              </tr>
             </tbody>
           </table>
         </div>

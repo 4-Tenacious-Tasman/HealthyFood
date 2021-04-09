a72c70edd05c4f53c569705eec2741bf5c06ddb7
Zachary Medellin <ZacharyMedellin44@gmail.com>
2021-04-08 23:56:46 -0700

    Remove imports of old photos

--- client/src/component/App.jsx
+++ client/src/component/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Main from './Main.jsx';
 import { NavLink, Switch, Route } from 'react-router-dom';
 import styles from './homepage/Homepage.module.css';
-import Logo from "../../public/images/HealthyFoodLogo.jpeg";
 import Navigation from './homepage/Navigation.jsx';
 
 

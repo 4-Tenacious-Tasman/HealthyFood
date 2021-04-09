14c8e13b0fab3300f880b1a3463b4aaa0b1bff86
Audree Steinberg <audreesteinberg@gmail.com>
2021-04-08 17:44:27 -0700

    Implement Protected Routes through higher order components to conditionally render pages based on user authentication status. Add spinner to display while authentication verification is loading

--- client/src/component/Main.jsx
+++ client/src/component/Main.jsx
@@ -8,23 +8,34 @@ import Login from './login/Login.jsx';
 import { useAuth0 } from "@auth0/auth0-react";
 import Navigation from './homepage/Navigation.jsx';
 import Profile from './login/Profile.jsx';
+import ProtectedRoute from './login/ProtectedRoute.jsx';
+import Loader from './login/Loader.jsx';
 
  const Main = () => {
 
-  const { user, isAuthenticated } = useAuth0()
+  const { user, isAuthenticated, isLoading } = useAuth0()
+
+  if (isLoading) {
+    return (
+        <div className="container h-100">
+          <div className="row h-100 justify-content-center align-items-center">
+              <Loader />
+          </div>
+        </div>
+    )
+  }
 
   return (
     <Router>
-      <Navigation />
-      <Switch>
-        <Route path='/marketplace' component={FarmersMarket}></Route>
-        <Route path='/userprofile'>
-          {!isAuthenticated ? <Redirect to='/login' /> : <Profile />}
-        </Route>
-        <Route path='/login' component={Login}></Route>
-        <Route path='/RecipeGenerator' component={RecipeGen}></Route>
-        <Route path='/' component={HomePage}></Route>
-      </Switch>
+        <Navigation />
+        <Switch>
+          <Route path='/marketplace' component={FarmersMarket}></Route>
+          {/* <Route path='/userprofile' render={() => <UserProfile userEmail={user.email} />}></Route> */}
+          <ProtectedRoute path='/userprofile' component={Profile} />
+          <Route path='/login' component={Login}></Route>
+          <Route path='/RecipeGenerator' component={RecipeGen}></Route>
+          <Route path='/' component={HomePage}></Route>
+        </Switch>
     </Router>
   )
 

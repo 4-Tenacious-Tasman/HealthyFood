7a261f1a429fe1a36339a8f3931b3568ac2c79f3
Anna Zhu <zeehaychoo@gmail.com>
2021-04-10 09:42:09 -0700

    Remove unutilized server endpoints and organize router file

--- server/controllers/profile/profile.js
+++ server/controllers/profile/profile.js
@@ -4,15 +4,15 @@ const db = require('../../../database/index.js');
 const { mealPlan, apiKey, recipes } = require('../../config.js');
 
 module.exports = {
-  updateSubscription: (req, res) => {
-    const { subscribed, id } = req.body;
-    db.query('UPDATE users SET subscribed = $1 WHERE id = $2 RETURNING *;', [subscribed, id])
-      .then(result => {
-        res.json(result.rows);
+  userPlans: (req, res) => {
+    const { id } = req.query;
+    db.query('SELECT * FROM daily_meal_plans WHERE user_id = $1;', [id])
+      .then(data => {
+        res.json(data.rows);
       })
       .catch(err => {
-        res.json(err);
-      })
+        res.send(err);
+      });
   },
   userDetails: (req, res) => {
     const { email } = req.query;
@@ -68,31 +68,11 @@ module.exports = {
         res.sendStatus(400);
       });
   },
-  signup: (req, res) => {
-    const { first_name, last_name, email, age, target_calories, diet, exclude } = req.body;
-    db.query('INSERT INTO users(first_name, last_name, email, age, target_calories, diet, exclude) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *;', [first_name, last_name, email, age, target_calories, diet, exclude])
-      .then(data => {
-        res.json(data.rows);
-      })
-      .catch(err => {
-        res.send(err);
-      });
-  },
   updatePreferences: (req, res) => {
     const { id, first_name, last_name, age, target_calories, diet, exclude } = req.body;
     db.query('UPDATE users SET first_name = $1, last_name = $2, age = $3, target_calories = $4, diet = $5, exclude = $6 WHERE id = $7 RETURNING *;', [first_name, last_name, age, target_calories, diet, exclude, id])
       .then(data => {
         res.json(data.rows[0]);
-      })
-      .catch(err => {
-        res.send(err);
-      });
-  },
-  userPlans: (req, res) => {
-    const { id } = req.query;
-    db.query('SELECT * FROM daily_meal_plans WHERE user_id = $1;', [id])
-      .then(data => {
-        res.json(data.rows);
       })
       .catch(err => {
         res.send(err);
@@ -135,5 +115,15 @@ module.exports = {
       .catch(() => {
         res.sendStatus(400);
       });
+  },
+  updateSubscription: (req, res) => {
+    const { subscribed, id } = req.body;
+    db.query('UPDATE users SET subscribed = $1 WHERE id = $2 RETURNING *;', [subscribed, id])
+      .then(result => {
+        res.json(result.rows);
+      })
+      .catch(err => {
+        res.json(err);
+      })
   }
 }
--- server/controllers/routes/routes.js
+++ server/controllers/routes/routes.js
@@ -3,13 +3,16 @@ const marketplace = require('../marketplace/marketplace.js');
 let profile = require('../profile/profile.js');
 
 let healthyfood = express.Router();
-healthyfood.get('/userPlans',profile.userPlans )
-healthyfood.get('/userDetails',profile.userDetails )
-healthyfood.post('/newPlan', profile.newPlan)
-healthyfood.post('/signup',profile.signup )
-healthyfood.put('/updatePreferences',profile.updatePreferences )
-healthyfood.put('/changePlan',profile.changePlan)
-healthyfood.get('/ingredients', marketplace.getAllIngredients);
+// User profile endpoints
+healthyfood.get('/userPlans',profile.userPlans);
+healthyfood.get('/userDetails',profile.userDetails);
+
+healthyfood.post('/newPlan', profile.newPlan);
+
+healthyfood.put('/updatePreferences',profile.updatePreferences);
+healthyfood.put('/changePlan',profile.changePlan);
 healthyfood.put('/updateSubscription', profile.updateSubscription);
+// Marketplace endpoints
+healthyfood.get('/ingredients', marketplace.getAllIngredients);
 
 module.exports.healthyfood = healthyfood;

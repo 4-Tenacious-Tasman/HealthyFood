960a55d4ffa74df460d13b6eee3b23d71febd48b
Anna Zhu <zeehaychoo@gmail.com>
2021-04-10 12:50:29 -0700

    Refactored user profile endpoints to modular separation of routes, controllers, and models.

--- server/controllers/profile/profile.js
+++ server/controllers/profile/profile.js
@@ -1,129 +1,99 @@
 const express = require('express');
 const axios = require('axios')
-const db = require('../../../database/index.js');
+const {
+  userPlans,
+  userDetails,
+  newPlan,
+  changePlan,
+  updatePreferences,
+  updateSubscription
+} = require('../../models/profile/models.js');
 const { mealPlan, apiKey, recipes } = require('../../config.js');
 
 module.exports = {
-  userPlans: (req, res) => {
-    const { id } = req.query;
-    db.query('SELECT * FROM daily_meal_plans WHERE user_id = $1;', [id])
-      .then(data => {
-        res.json(data.rows);
+  response: (err, data, res) => {
+    if (err) {
+      console.log('err');
+      res.status(404).json(err);
+    }
+    console.log('success');
+    res.status(200).json(data);
+  },
+  generateMealPlan: (parameters, cb) => {
+    let { timeFrame, targetCalories, diet, exclude } = parameters;
+    axios.get(`${mealPlan}`, {
+      params: {
+        apiKey,
+        timeFrame,
+        targetCalories,
+        diet,
+        exclude
+      }
+    })
+      .then(result => {
+        const { meals, nutrients } = result.data;
+        parameters.nutrients = nutrients;
+        meals.forEach(async (meal, index) => {
+          if (index === 0) {
+            parameters.breakfast = meal;
+          } else if (index === 1) {
+            parameters.lunch = meal;
+          } else {
+            parameters.dinner = meal;
+          }
+        });
+        cb(null, parameters);
       })
       .catch(err => {
-        res.send(err);
+        cb(err, null);
       });
   },
+  userPlans: (req, res) => {
+    userPlans(req.query.id, (err, userPlans) => {
+      module.exports.response(err, userPlans, res);
+    });
+  },
   userDetails: (req, res) => {
-    const { email } = req.query;
-    db.query('SELECT * FROM users WHERE email IN ($1)', [email])
-      .then(result => {
-        if (!result.rows.length > 0) {
-          db.query('INSERT INTO users(email) VALUES($1) RETURNING *;', [email])
-            .then(userDetails => {
-              res.json(userDetails.rows[0]);
-            })
-        } else {
-          res.json(result.rows[0]);
-        }
-      })
-      .catch(err => {
-        res.send(err);
-      })
+    userDetails(req.query.email, (err, userDetails) => {
+      module.exports.response(err, userDetails, res);
+    });
   },
   newPlan: (req, res) => {
     const { date, id, target_calories, diet, exclude } = req.body;
     const timeFrame = 'day';
     const targetCalories = target_calories;
-    axios.get(`${mealPlan}`, {
-      params: {
-        apiKey,
-        timeFrame,
-        targetCalories,
-        diet,
-        exclude
+    const apiParams = { timeFrame, targetCalories, diet, exclude, id, date };
+    module.exports.generateMealPlan(apiParams, async (err, userDetails) => {
+      if (err) {
+        res.status(404).json(err);
       }
-    })
-      .then(result => {
-        const { meals, nutrients } = result.data;
-        let breakfast, lunch, dinner;
-        meals.forEach(async (meal, index) => {
-          if (index === 0) {
-            breakfast = meal;
-          } else if (index === 1) {
-            lunch = meal;
-          } else {
-            dinner = meal;
-          }
-        });
-        db.query('INSERT INTO daily_meal_plans(user_id, date, breakfast, lunch, dinner, nutrients) VALUES ($1, $2, $3, $4, $5, $6);', [id, date, breakfast, lunch, dinner, nutrients])
-          .then(() => {
-            db.query('SELECT * FROM daily_meal_plans WHERE user_id = $1;', [id])
-              .then(data => {
-                res.status(200).json(data.rows);
-              })
-          });
-      })
-      .catch(() => {
-        res.sendStatus(400);
+      newPlan(userDetails, (err, newPlan) => {
+        module.exports.response(err, newPlan, res);
       });
+    });
   },
   changePlan: (req, res) => {
     const { meal_id, id, target_calories, diet, exclude } = req.body;
     const timeFrame = 'day';
     const targetCalories = target_calories;
-    axios.get(`${mealPlan}`, {
-      params: {
-        apiKey,
-        timeFrame,
-        targetCalories,
-        diet,
-        exclude
+    const apiParams = { timeFrame, targetCalories, diet, exclude, meal_id, id };
+    module.exports.generateMealPlan(apiParams, async (err, userDetails) => {
+      if (err) {
+        res.status(404).json(err);
       }
-    })
-      .then(result => {
-        const { meals, nutrients } = result.data;
-        let breakfast, lunch, dinner;
-        meals.forEach(async (meal, index) => {
-          if (index === 0) {
-            breakfast = meal;
-          } else if (index === 1) {
-            lunch = meal;
-          } else {
-            dinner = meal;
-          }
-        });
-
-        db.query('UPDATE daily_meal_plans SET breakfast = $1, lunch = $2, dinner = $3, nutrients = $4 WHERE id = $5;', [breakfast, lunch, dinner, nutrients, meal_id])
-          .then(() => {
-            db.query('SELECT * FROM daily_meal_plans WHERE user_id = $1;', [id])
-              .then(data => {
-                res.status(200).json(data.rows);
-              })
-          });
-      })
-      .catch(() => {
-        res.sendStatus(400);
+      changePlan(userDetails, (err, updatedPlan) => {
+        module.exports.response(err, updatedPlan, res);
       });
+    });
   },
   updatePreferences: (req, res) => {
-    const { id, first_name, last_name, age, target_calories, diet, exclude } = req.body;
-    db.query('UPDATE users SET first_name = $1, last_name = $2, age = $3, target_calories = $4, diet = $5, exclude = $6 WHERE id = $7 RETURNING *;', [first_name, last_name, age, target_calories, diet, exclude, id])
-      .then(data => {
-        res.json(data.rows[0]);
-      })
-      .catch(err => {
-        res.send(err);
-      });
+    updatePreferences(req.body, (err, updatedPreferences) => {
+      module.exports.response(err, updatedPreferences, res);
+    });
   },
   updateSubscription: (req, res) => {
-    const { subscribed, id } = req.body;
-    db.query('UPDATE users SET subscribed = $1 WHERE id = $2 RETURNING *;', [subscribed, id])
-      .then(result => {
-        res.json(result.rows);
-      })
-      .catch(err => {
-        res.json(err);
-      })
+    updateSubscription(req.body, (err, updatedSubscription) => {
+      module.exports.response(err, updatedSubscription, res);
+    });
   }
 }
